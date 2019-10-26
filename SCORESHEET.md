3df636f6f48ee05119aa9a3c6c727f6d583809a5
Chao Hsu Lin <chao0226@uw.edu>
2019-10-25 23:05:59 -0700

    Push all files from turnin master

---
+++ SCORESHEET.md
@@ -0,0 +1,34 @@
+# Scoresheet for Creative Project 2
+Commit SHA: fda672cbd5823165403377e1a849eeea362ef311
+
+**7.5 / 8 : Total Score**
+- **3.0 / 3.0 : External Correctness**
+     - 1 / 1 : Responds to page UI/mouse/keyboard event
+     - 1 / 1 : Changes DOM using appendChild, removeChild, or replaceChild
+     - 0.5 / 0.5 : Dynamically modifies classList with JS
+     - 0.5 / 0.5 : Creates at least 1 flex container
+- **3.5 / 4.0 : Internal Correctness**
+     - 0.5 / 0.5 : Three files included (correctly)
+     - 0.5 / 0.5 : Appropriate separation of Content JS and HTML
+     - 0.5 / 0.5 : Otherwise good HTML Code Quality
+     - 0.5 / 1 : Passes JS Linter & Otherwise Good JS Code Quality
+          - Great job turning in JS code which passes the linter! Make sure to keep doing this for all future assignments.
+          - -0.5: Broken link. See `index.js`, line 95 for details.
+     - 0.5 / 0.5 : Passes CSS Linter
+          - Great job turning in CSS code which passes the linter! Make sure to keep doing this for all future assignments.
+     - 0.5 / 0.5 : Passes HTML Validator
+          - Great job turning in HTML code which passes the validator! Make sure to keep doing this for all future assignments.
+     - 0.5 / 0.5 : Reduces CSS Redundancy
+          - Great job turning in CSS with no redundancy! Make sure to keep doing this for all future assignments.  We have a really handy [CSS Redundancy Checker](https://courses.cs.washington.edu/courses/cse154/19au/resources/assets/css-redundancy-checker/index.html) that you may find helpful.
+- **1.0 / 1.0 : Overall Code Quality and Documentation**
+     - 0.5 / 0.5 : Complete header comment - HTML/CSS 
+     - 0.5 / 0.5 : Complete header comment - JS file documentation
+- **0 : Lateness and Other Deductions**
+     - Due: 10/16/2019 11:00 PM
+     - Submitted: 10/15/2019 08:20 PM (on time)
+     - 0 : Late days used on this assignment
+     - 0 : Lateness deduction
+     - 0 : Other deductions
+
+**Overall Comments:**
+Great job Ariane! My favorite starter is Oshawott. Also the page is beautiful! Make sure to check the file comments for detailed feedback!
---
+++ index.js
@@ -0,0 +1,139 @@
+/*
+ *  Name: Ariane Apigo
+ *  Date: 2019 October 11
+ *  Section: AA / Chao Hsu Lin & Austin Jenchi
+ *
+ *  This is the index.js file for my PokéPortal site. It enables the game / simulator to guide
+ *  the user through the experience of receiving their first Pokémon (a starter Pokémon).
+ *  All Pokémon photos were found at https://bulbapedia.bulbagarden.net/wiki/ and are licensed
+ *  under fair use.
+ */
+"use strict";
+(function() {
+
+  window.addEventListener("load", init);
+  const GENERATIONS = 7;
+
+  /**
+   * Sets up event-listeners on buttons and images.
+   */
+  function init() {
+    document.getElementById("start").addEventListener("click", start);
+    document.getElementById("next").addEventListener("click", next);
+    document.getElementById("fire").addEventListener("click", choose);
+    document.getElementById("grass").addEventListener("click", choose);
+    document.getElementById("water").addEventListener("click", choose);
+  }
+
+  /**
+   * Changes view to first "stage;" starts the game / simulator.
+   */
+  function start() {
+    let portal = document.querySelectorAll("section");
+    for (let i = 0; i < portal.length; i++) {
+      if (portal[i].classList.contains("hidden")) {
+        portal[i].classList.toggle("hidden");
+      } else {
+        portal[i].classList.add("hidden");
+      }
+    }
+    let body = document.querySelector("body");
+    body.classList.add("home");
+  }
+
+  /**
+   * Changes the view to the next "stage;" continues the game / simulator.
+   * Note that this works because there are only two stages to the game / simulator.
+   */
+  function next() {
+    let stages = document.querySelectorAll("article");
+    for (let i = 0; i < stages.length; i++) {
+      if (stages[i].classList.contains("hidden")) {
+        stages[i].classList.toggle("hidden");
+      } else {
+        stages[i].classList.add("hidden");
+      }
+    }
+  }
+
+  /**
+   * Chooses the user's starter Pokémon based on their type choice.
+   * Also shows the final dialogue of the game / simulator.
+   */
+  function choose() {
+    let type = this.id;
+    let ballDiv = document.getElementById("pokeballs");
+    let chosenPoke = document.createElement("img");
+    if (type === "fire") {
+      chosenPoke.src = fire();
+      ballDiv.insertBefore(chosenPoke, ballDiv.firstChild);
+      ballDiv.removeChild(ballDiv.children[1]);
+      document.getElementById("grass").removeEventListener("click", choose);
+      document.getElementById("water").removeEventListener("click", choose);
+    } else if (type === "grass") {
+      chosenPoke.src = grass();
+      ballDiv.insertBefore(chosenPoke, ballDiv.children[1]);
+      ballDiv.removeChild(ballDiv.children[2]);
+      document.getElementById("fire").removeEventListener("click", choose);
+      document.getElementById("water").removeEventListener("click", choose);
+    } else {
+      chosenPoke.src = water();
+      ballDiv.appendChild(chosenPoke);
+      ballDiv.removeChild(ballDiv.children[2]);
+      document.getElementById("fire").removeEventListener("click", choose);
+      document.getElementById("grass").removeEventListener("click", choose);
+    }
+    chosenPoke.alt = "Your starter Pokémon.";
+    endDialogue();
+  }
+
+  /**
+   * Creates path to images of fire-type starter Pokémon and chooses one from a random generation.
+   * @returns {string} image path to random fire-type starter Pokémon
+   */
+  function fire() {
+    let path = "img/fire/";
+    let chosenGen = Math.floor(Math.random() * GENERATIONS);
+    let choices = ["Charmander", "Chimchar", "Cyndaquil", "Fennekin",
+        "Litten", "Tepig", "Torchic"];
+    return path + choices[chosenGen] + ".png";
+  }
+
+  /**
+   * Creates path to images of grass-type starter Pokémon and chooses one from a random generation.
+   * @returns {string} image path to random grass-type starter Pokémon
+   */
+  function grass() {
+    let path = "img/grass/";
+    let chosenGen = Math.floor(Math.random() * GENERATIONS);
+    let choices = ["Bulbasaur", "Chespin", "Chikorita", "Rowlet",
+        "Snivy", "Treecko", "Turtwig"];
+    return path + choices[chosenGen] + ".png";
+  }
+
+  /**
+   * Creates path to images of water-type starter Pokémon and chooses one from a random generation.
+   * @returns {string} image path to random water-type starter Pokémon
+   */
+  function water() {
+    let path = "img/water/";
+    let chosenGen = Math.floor(Math.random() * GENERATIONS);
+    let choices = ["Froakie", "Mudkip", "Oshawott", "Piplup",
+        "Popplio", "Squirtle", "Totodile"];
+    return path + choices[chosenGen] + ".png";
+  }
+
+  /**
+   * Shows the final dialogue of the game / simulator.
+   */
+  function endDialogue() {
+    let speechDialogues = document.querySelectorAll(".speech");
+    for (let i = 0; i < speechDialogues.length; i++) {
+      if (!speechDialogues[i].classList.contains("hidden")) {
+        speechDialogues[i].classList.add("hidden");
+      }
+    }
+    document.getElementById("endDialogue").classList.remove("hidden");
+  }
+
+})();
